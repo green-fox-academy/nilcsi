b39d988ea11a81b008ade305088b866004f5e4ea
nilcsi <nilcsi@gmail.com>
2018-10-29 12:39:21 +0100

    First project

---
+++ week-02/day-1/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName ="Nilofer";
+        int myAge = 26;
+        double myHeight = 1.57;
+        boolean myCondition = false;
+
+        System.out.println("My name is " + myName);
+        System.out.println("I'm " + myAge + " years old");
+        System.out.println("I'm " + myHeight + " cm");
+        System.out.println("My condition: " + myCondition);
+    }
+}
---
+++ week-02/day-1/src/Swap.java
@@ -0,0 +1,14 @@
+public class Swap {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+
+        int c = a;
+        int a2 = b;
+        int b2 = c;
+
+        System.out.println("a = " + a2);
+        System.out.println("b = " + b2);
+    }
+}
