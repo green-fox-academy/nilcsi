ac452b0a17d38dab26e0718f5d9a3df2b24c22b8
nilcsi <nilcsi@gmail.com>
2018-11-06 12:57:03 +0100

    MatchMaking and Reverse

--- week-03/day-2/src/MatchMaking.java
+++ week-03/day-2/src/MatchMaking.java
@@ -2,32 +2,18 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class MatchMaking{
-    public static void main(String... args){
+   public static void main(String... args){
         ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
         ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
-
-        for(int i = 0; i < girls.size(); i++) {
-            System.out.print(girls.get(i) + " ");
-            for (int j = 0; j < boys.size() + 1; j++) {
-                System.out.print(boys.get(i) + " ");
-            }
-        }
-
 
         // Write a method that joins the two lists by matching one girl with one boy into a new list
         // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
 
-        //  System.out.println(makingMatches(girls, boys));
+        System.out.println(makingMatches(girls, boys));
     }
 
-  /*  public static String makingMatches(ArrayList<String> girls, ArrayList<String> boys) {
-         for(int i = 0; i < girls.size(); i++) {
-             System.out.println(girls.get(i));
-             for (int j = 0; j < boys.size(); j++) {
-                 System.out.println(boys.get(i));
-             }
+    public static String makingMatches(ArrayList<String> girls, ArrayList<String> boys) {
+       
 
-         }
-         return ;
-    }*/
+   }
 }
--- week-03/day-2/src/Reverse.java
+++ week-03/day-2/src/Reverse.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.lang.String;
+
 public class Reverse {
     public static void main(String... args){
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
@@ -6,14 +9,18 @@ public class Reverse {
         // Use it on this reversed string to check it!
         // Try to solve this using charAt() first, and optionally anything else after.
 
+          // System.out.println(original);
+        reverse(reversed);
+        System.out.println(reverse(reversed));
+    }
+
+    public static String reverse(String reversed) {
         String original = "";
         int length = reversed.length();
 
-        for ( int i = length - 1 ; i >= 0 ; i-- ) {
+        for (int i = length - 1; i >= 0; i--) {
             original = original + reversed.charAt(i);
         }
-        System.out.println(original);
-
-        //   System.out.println(reverse(reversed));
+        return original;
     }
 }
