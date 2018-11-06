957db832e72f5764180fc0e8fff5c62b19c3fe26
nilcsi <nilcsi@gmail.com>
2018-11-06 14:30:21 +0100

    AppendLetter

--- week-03/day-2/src/AppendLetter.java
+++ week-03/day-2/src/AppendLetter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class AppendLetter{
- /*   public static void main(String... args){
+public class AppendLetter {
+    public static void main(String... args) {
         List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
         // Create a method called "appendA()" that adds "a" to every string in the "far" list.
         // The parameter should be a list.
@@ -9,15 +9,13 @@ public class AppendLetter{
         System.out.println(appendA(far));
     }
 
-    public static String appendA(List<String> far){
+    public static List<String> appendA(List<String> newList) {
 
-        String[] tomb = far.toArray();
-
-        for (int i = 0; i < tomb.length(); i++) {
-            tomb[i] = far.add("a");
-            System.out.println(tomb[i]);
+        for (int i = 0; i < newList.size(); i++) {
+            newList.set(i, newList.get(i) + "a");
         }
-    }*/
+        return newList;
+    }
 }
 
 // The output should be: "boa", "anaconda", "koala", "panda", "zebra"
