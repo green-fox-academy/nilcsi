a235620df44ab6bafa1c7df0cbc8256aee8d222d
nilcsi <nilcsi@gmail.com>
2018-11-08 15:11:19 +0100

    Draw Project

---
+++ week-03/day-5/src/envelopeStar.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class envelopeStar {
+
+    public static void mainDraw(Graphics graphics){
+
+        int num = 13;
+
+        for (int i = 0; i < num; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(WIDTH / 2, i * num, WIDTH / 2 + i * num, HEIGHT / 2);
+            graphics.drawLine(WIDTH / 2, i * num, WIDTH / 2 - i * num, HEIGHT / 2);
+            graphics.drawLine(WIDTH / 2, HEIGHT - (i * num), WIDTH / 2 + (i * num), HEIGHT / 2);
+            graphics.drawLine(WIDTH / 2, HEIGHT - (i * num), WIDTH / 2 - (i * num), HEIGHT / 2);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week-03/day-5/src/linePlayQuarters.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class linePlayQuarters {
+    public static void mainDraw(Graphics graphics){
+
+        graphics.setColor(Color.GREEN);
+        for (int i = WIDTH/15; i < WIDTH - WIDTH/15; i += WIDTH / 15) {
+            graphics.drawLine(i, HEIGHT, 0, HEIGHT - WIDTH + i);
+        }
+        graphics.setColor(Color.MAGENTA);
+        for (int i = WIDTH - WIDTH/15; i > WIDTH/15; i -= WIDTH / 15) {
+            graphics.drawLine(i, 0, WIDTH, HEIGHT - WIDTH + i);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        envelopeStar.ImagePanel panel = new envelopeStar.ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
