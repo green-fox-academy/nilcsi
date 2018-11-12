2d06366c07aaf936da14cfa88d05dd23f0deb172
nilcsi <nilcsi@gmail.com>
2018-11-12 12:01:16 +0100

    PrintEachLine

--- week-04/day-1/src/PrintEachLine.java
+++ week-04/day-1/src/PrintEachLine.java
@@ -1,6 +1,7 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class PrintEachLine {
     public static void main(String[] args) {
@@ -9,6 +10,14 @@ public class PrintEachLine {
         // If the program is unable to read the file (for example it does not exists),
         // then it should print an error message like: "Unable to read file: my-file.txt"
 
-
+        try {
+            Path filePath = Paths.get("source\\my-file.txt");
+            List<String> lines = Files.readAllLines(filePath);
+            for(int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+            }
+        } catch (Exception e) {
+            System.out.println("Could not read the file!");
+        }
     }
 }
