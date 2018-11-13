5bdf2c27d51c5f8f4dd6f3c8af3971bccf86a349
nilcsi <nilcsi@gmail.com>
2018-11-13 11:40:10 +0100

    PostIt

---
+++ week-04/day-2/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        PostIt idea1 = new PostIt("orange", "Idea 1", "blue");
+        PostIt awesome = new PostIt("pink", "Awesome", "black");
+        PostIt suberb = new PostIt("yellow", "Superb", "green");
+        
+    }
+}
---
+++ week-04/day-2/src/com/company/PostIt.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class PostIt {
+
+    String setBackground;
+    String text;
+    String setFont;
+
+    PostIt(String setBackground, String text, String setFont) {
+        this.setBackground = setBackground;
+        this.text = text;
+        this.setFont = setFont;
+    }
+
+}
